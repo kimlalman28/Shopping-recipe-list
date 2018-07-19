c8d31a1ce03bb4f889a22320ef12fa48351a2483
Kimberly Lalmansingh <kimlalman28@gmail.com>
2018-07-19 01:33:52 -0400

    added new components

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipes } from '../recipes.model';
+
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-	recipes = [];
+	recipes: Recipes[] = [
+		new Recipes('Pie', 'Dessert', 'http://www.stock-free.org/images/Thanksgiving-Stock-Free-Image-08112015-image-133.jpg'),
+		new Recipes('Pie', 'Dessert', 'http://www.stock-free.org/images/Thanksgiving-Stock-Free-Image-08112015-image-133.jpg'),
+		new Recipes('Pie', 'Dessert', 'http://www.stock-free.org/images/Thanksgiving-Stock-Free-Image-08112015-image-133.jpg'),
+		new Recipes('Pie', 'Dessert', 'http://www.stock-free.org/images/Thanksgiving-Stock-Free-Image-08112015-image-133.jpg'),
+
+	];
 
   constructor() { }
 
---
+++ src/app/recipes/recipes.model.ts
@@ -0,0 +1,11 @@
+export class Recipes {
+	public name: string;
+	public description: string;
+	public imagePath: string;
+
+	constructor(name:string, desc:string, img:string) {
+		this.name = name;
+		this.description = desc;
+		this.imagePath = img;
+	}
+}
